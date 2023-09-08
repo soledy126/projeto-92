7cdb5210f514ef1c4e99df8a74b407a4e7947658
soledy126 <132954618+soledy126@users.noreply.github.com>
2023-09-08 14:49:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('AniversarioImagem.png', function(Tag) {
+    blockImageObject = Img;
+    blockImageObject.scaleToWIdth(700);
+    blockImageObject.scaleToHeight(510);
+    blockImageObject.set({
+        top:0,
+        left:0
+    });
+    canvas.add(blockImageObject);
+})
+
+function playSound(){
+    x.play();
+}
